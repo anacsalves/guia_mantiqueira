a2938dc507f2ebaf8f830acf8775f4c6c9cbc82d
Ana Clara <anacsalves@unifei.edu.br>
2024-11-23 21:10:17 -0300

    adição da pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "./components/header";
 import home from "../assets/img/homeimg.png"
 import Footer from "./components/footer";
+import TelaInicial from "./components/tela-inicial";
 
 
 export default function Home() {
   return (
    <>
     <Header />
+    <TelaInicial/>
     <Footer />
    </>
   );
